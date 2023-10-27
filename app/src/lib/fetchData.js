91cfcb0a51f6ab2602ef706df661c66b4293ae68
Anand Bawa <anand.bawa@yahoo.com>
2023-10-28 01:04:55 +0530

    commits

--- app/src/lib/fetchData.js
+++ app/src/lib/fetchData.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // axios custom fetch function to query the server
 const fetchData = axios.create({
   baseURL: "/api/v1",
+  withCredentials: true,
 });
 
 export default fetchData;
