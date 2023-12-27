e2381ce736b6ad40a31fa85cfd4e04a484d817c2
Anand Bawa <anand.bawa@yahoo.com>
2023-12-27 11:43:21 +0530

    revert

--- app/src/pages/LandingPage.jsx
+++ app/src/pages/LandingPage.jsx
@@ -1,21 +1,10 @@
-import { Categories, Loading, MaxDiscount } from "@/components";
-import { Loader2 } from "lucide-react";
-import { lazy, Suspense } from "react";
-const Hero = lazy(() => import("@/components"));
+import { Hero, Categories, MaxDiscount } from "@/components";
 
 // Landing page of the website
 const LandingPage = () => {
   return (
     <div className="px-2 mx-auto w-full md:max-w-[80vw]">
-      <Suspense
-        fallback={
-          <div className="grid place-items-center">
-            <Loader2 className="text-primary h-24 w-24 animate-spin" />
-          </div>
-        }
-      >
-        <Hero />
-      </Suspense>
+      <Hero />
       <MaxDiscount />
       <Categories />
     </div>
